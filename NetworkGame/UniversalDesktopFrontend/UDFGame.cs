b8c72234281bca9ef3adcc6b822a9e8a04b3e427
uu3474 <uu3474@gmail.com>
2020-09-14 12:42:22 +0300

    Little fixes

--- NetworkGame/UniversalDesktopFrontend/UDFGame.cs
+++ NetworkGame/UniversalDesktopFrontend/UDFGame.cs
@@ -3,9 +3,6 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace UniversalDesktopFrontend
 {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
     public class UDFGame : Game
     {
         GraphicsDeviceManager m_graphics;
@@ -32,23 +29,12 @@ namespace UniversalDesktopFrontend
             this.IsMouseVisible = true;
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
             m_networkGame = new NetworkGame.Game(GraphicsDevice, Content);
-
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
             var _params = new NetworkGame.GameParams();
@@ -64,35 +50,20 @@ namespace UniversalDesktopFrontend
             m_networkGame.Init(_params);
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
         protected override void UnloadContent()
         {
             m_networkGame.SaveDataIfNeed();
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
             m_networkGame.Update(gameTime);
-
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
             m_networkGame.Draw(gameTime);
-
             base.Draw(gameTime);
         }
     }
